4e2681a3c96047a6060816846e1f4edc5254c4fc
Kristian Kielhofner <kris@kriskinc.com>
2020-04-26 14:57:10 -0500

    Move get_pretrained into train and manage pip better

--- get_pretrained.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/NVIDIA/retinanet-examples/releases/download/19.04/retinanet_rn34fpn.zip
-
-unzip -j retinanet_rn34fpn.zip
--- train.sh
+++ train.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-pip install --no-cache-dir .
-pip install tensorboardX
-
-if [ ! -f "retinanet_rn34fpn.pth" ]; then
-  ./get_pretrained.sh
+# Nasty hack because pip list and pipes are busted
+if [ ! -r pip_deps_installed ]; then
+  pip install --no-cache-dir .
+  pip install tensorboardX
+  touch pip_deps_installed
 fi
 
-# Default to batch 30 for two Nvidia 1080 Tis
+if [ ! -r retinanet_rn34fpn.pth ]; then
+  wget https://github.com/NVIDIA/retinanet-examples/releases/download/19.04/retinanet_rn34fpn.zip
+  unzip -j retinanet_rn34fpn.zip
+fi
+
 if [ "$1" ]; then
   BATCH="$1"
 else
   BATCH="30"
 fi
 
-# Iterations have been changed to 30000 with milestones adjusted accordingly
-retinanet train redaction.pth --backbone ResNet34FPN  \
+rm -rf logs
+mkdir -p logs
+
+time retinanet train redaction.pth --backbone ResNet34FPN  \
     --fine-tune retinanet_rn34fpn.pth  --classes 1 --lr 0.00003  \
     --batch "$BATCH" --images /data/open_images/train_faces  \
     --annotations /data/open_images/train_faces.json  \
@@ -23,3 +29,7 @@ retinanet train redaction.pth --backbone ResNet34FPN  \
     --val-annotations /data/open_images/val_faces.json --val-iters 3000  \
     --resize 800  --max-size 880 --iters 30000 \
     --milestones 15000 22500 --logdir logs/ | tee redaction.log
+
+if [ -r redaction.pth ]; then
+  time retinanet export redaction.pth redaction.onnx --size 512 864 --batch 4
+fi
